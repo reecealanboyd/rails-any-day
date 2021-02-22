df3ba4cb575185e6fe6e937dbbb590f64ef2e38b
Reece Boyd <reecealanboyd@users.noreply.github.com>
2021-02-21 20:28:12 -0600

    users, auth, session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,22 @@
 
 class ApplicationController < ActionController::Base
   around_action :switch_locale
+  helper_method :current_user
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
 
   def switch_locale(&action)
     locale = params[:locale] || I18n.default_locale
     I18n.with_locale(locale, &action)
+  end
+
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id])
+    else
+      @current_user = nil
+    end
   end
 end
